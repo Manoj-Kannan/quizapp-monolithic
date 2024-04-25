fcd987f47ff07a3e3b35ad85b584338e1511cef8
ManojKannanD <fabulousmanoj27@gmail.com>
2024-04-25 23:45:28 +0530

    added getById in quiz controller
    created QuestionWrapper to skip correctAnswer, difficulty in Quiz Context

--- src/main/java/com/example/quizapp/service/QuizService.java
+++ src/main/java/com/example/quizapp/service/QuizService.java
@@ -1,6 +1,7 @@
 package com.example.quizapp.service;
 
 import com.example.quizapp.model.Question;
+import com.example.quizapp.model.QuestionWrapper;
 import com.example.quizapp.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.quizapp.dao.QuestionDao;
@@ -9,7 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -30,6 +33,22 @@ public class QuizService {
         quizDao.save(quiz);
 
         return new ResponseEntity<>("success", HttpStatus.CREATED);
+
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+
+        if (quiz.isPresent()) {
+            List<Question> questionsFromDB = quiz.get().getQuestions();
+            for (Question q : questionsFromDB) {
+                QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+                questionsForUser.add(qw);
+            }
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
 
     }
 }
