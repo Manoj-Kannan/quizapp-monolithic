86a360a4f173aba913b5604797210eacf86626ed
ManojKannanD <fabulousmanoj27@gmail.com>
2024-04-25 23:34:15 +0530

    created simple quiz
    quiz & questions are mapped as Many-To-Many

--- src/main/java/com/example/quizapp/dao/QuestionDao.java
+++ src/main/java/com/example/quizapp/dao/QuestionDao.java
@@ -1,6 +1,7 @@
 package com.example.quizapp.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.example.quizapp.model.Question;
 
@@ -11,4 +12,6 @@ public interface QuestionDao extends JpaRepository<Question, Integer> {
 
     List<Question> findByCategory(String category);
 
+    @Query(value = "SELECT * FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    List<Question> findRandomQuestionsByCategory(String category, int numQ);
 }
